1f42ba7dc5b0e988db6aeeb0805ad7c9912a4b56
sameer <s.faridi007@gmail.com>
2026-09-02 20:44:52 +0530

    fix: correct order of BASE URL assignment in api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
-const BASE =  "http://localhost:5001";
-// const BASE = process.env.NEXT_PUBLIC_API_URL ||  "http://localhost:5000";
+// const BASE =  "http://localhost:5001";
+const BASE = process.env.NEXT_PUBLIC_API_URL ||  "http://localhost:5000";
 // backend base url
 export class ApiError extends Error {
   status: number;
